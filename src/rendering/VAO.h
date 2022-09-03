b252e65da95e32ed344f1c776b652c2680101693
MetaMechanists <percy.baker.0@gmail.com>
2022-09-03 22:14:13 +0100

    Refined VAO

--- src/rendering/VAO.h
+++ src/rendering/VAO.h
@@ -5,11 +5,10 @@
 
 
 
-// VBO = Vertex Buffer Object
 class VAO {
 private:
     unsigned int vbo;
-    unsigned int id;
+    unsigned int vao;
     unsigned int vertexCount;
 
 public:
@@ -18,16 +17,11 @@ public:
     auto Init() -> void;
 
     auto Bind() const -> void;
-    auto Unbind() const -> void;
+    static auto Unbind() -> void;
 
-    // describe an attribute of a vertex
-    // for example, an attribute could be added to indicate that the first 3 numbers in a vertex are floats, and represent the position of the vertex
     auto AddVertexAttribute(const VertexAttribute &attribute) const -> void;
-
-    // set the data inside the VBO
-    // vectors are easier to manipulate, safer, and more modern than C-style arrays, which is why they're used here
     auto Data(const vector<VERTEX_DATA_TYPE> &data, unsigned int vertexCount, unsigned int mode) -> void;
 
-    auto Render() -> void;
+    auto Render() const -> void;
 
 };
