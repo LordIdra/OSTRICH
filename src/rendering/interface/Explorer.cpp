2cddb434a6848e78fb80aa6d73fdaa35405565ba
MetaMechanists <percy.baker.0@gmail.com>
2022-11-02 23:29:19 +0000

    Added explorer sorting

--- src/rendering/interface/Explorer.cpp
+++ src/rendering/interface/Explorer.cpp
@@ -1,5 +1,8 @@
 #include "Explorer.h"
 
+#include <algorithm>
+#include <ios>
+#include <iostream>
 #include <main/Bodies.h>
 #include <depend/IconsMaterialDesignIcons_c.h>
 
@@ -21,19 +24,21 @@ namespace ScenarioExplorer {
         const string ICON_FONT_PATH = "../resources/fonts/material-design-icons.ttf";
 
         const int NAME_WIDTH = 150;
-        const int MASS_WIDTH = 100;
-        const int SPEED_WIDTH = 100;
-
+        const int MASS_WIDTH = 155;
         const float FONT_SIZE = 20.0F;
 
-        const ImVec2 WINDOW_SIZE = ImVec2(410, 1000);
-        const ImVec2 EXPLORER_SIZE = ImVec2(400, 600);
+        const unsigned int NAME_COLUMN_ID = 0;
+        const unsigned int MASS_COLUMN_ID = 1;
+
+        const ImVec2 WINDOW_SIZE = ImVec2(320, 1000);
+        const ImVec2 EXPLORER_SIZE = ImVec2(300, 600);
         
         const ImWchar ICON_RANGE[] = {ICON_MIN_MDI, ICON_MAX_MDI, 0}; // NOLINT (interfacing with C library)
 
         const ImGuiWindowFlags WINDOW_FLAGS = ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-        const ImGuiTableFlags EXPLORER_FLAGS = ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_ScrollY;
-        const ImGuiTableFlags EXPLORER_COLUMN_FLAGS = ImGuiTableColumnFlags_WidthFixed;
+        const ImGuiTableFlags EXPLORER_FLAGS = ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_Sortable | ImGuiTableFlags_ScrollY | ImGuiTableFlags_NoPadOuterX | ImGuiTableFlags_NoPadInnerX;
+
+        ImGuiTableSortSpecs* sortSpecs;
         
         bool windowOpen = true;
 
@@ -58,12 +63,16 @@ namespace ScenarioExplorer {
             string speedColumn = ICON_MDI_SPEEDOMETER_SLOW + string(" Speed");
 
             ImGui::TableSetupScrollFreeze(0, 1);
-            ImGui::TableSetupColumn(nameColumn.c_str(), EXPLORER_COLUMN_FLAGS, NAME_WIDTH);
-            ImGui::TableSetupColumn(massColumn.c_str(), EXPLORER_COLUMN_FLAGS, MASS_WIDTH);
-            ImGui::TableSetupColumn(speedColumn.c_str(), EXPLORER_COLUMN_FLAGS, SPEED_WIDTH);
+            ImGui::TableSetupColumn(nameColumn.c_str(), ImGuiTableColumnFlags_WidthFixed | ImGuiTableColumnFlags_DefaultSort, NAME_WIDTH, NAME_COLUMN_ID);
+            ImGui::TableSetupColumn(massColumn.c_str(), ImGuiTableColumnFlags_WidthStretch | ImGuiTableColumnFlags_PreferSortDescending, MASS_WIDTH, MASS_COLUMN_ID);
             ImGui::TableHeadersRow();
 
             ImGui::PopFont();
+        }
+
+        auto AddAllBodies(vector<string> &bodyIds) -> void {
+            for (const auto &pair : Bodies::GetMassiveBodies())  { bodyIds.push_back(pair.first); }
+            for (const auto &pair : Bodies::GetMasslessBodies()) { bodyIds.push_back(pair.first); }
         }
 
         auto AddNameSelectable(const string &id, const string &name) -> void {
@@ -83,45 +92,64 @@ namespace ScenarioExplorer {
             ImGui::Text("%s", "--------");
         }
 
-        auto AddVelocityText(const vec3 velocity) -> void {
-            ImGui::TableNextColumn();
-            ImGui::Text("%.2e", glm::length(velocity));
-        }
-
-        auto AddMassiveBodies() -> void {
+        auto AddBodiesToTable(const vector<string> &bodyIds) -> void {
             ImGui::PushFont(dataFont);
-
-            for (const auto &pair : Bodies::GetMassiveBodies()) {
-                AddNameSelectable(pair.first, pair.second.GetName());
-                AddMassiveMassText(pair.second.GetMass());
-                AddVelocityText(pair.second.GetVelocity());
+            for (const string id : bodyIds) {
+                Body body = Bodies::GetBody(id);
+                AddNameSelectable(id, body.GetName());
+                if (body.GetMass() == 0) {
+                    AddMasslessMassText();
+                } else {
+                    AddMassiveMassText(body.GetMass());
+                }
             }
-
             ImGui::PopFont();
         }
 
-        auto AddMasslessBodies() -> void {
-            ImGui::PushFont(dataFont);
+        auto CompareBodyNames(string id1, string id2) -> bool {           
+            for (char &c : id1) { c = toupper(c); }
+            for (char &c : id2) { c = toupper(c); }
 
-            for (const auto &pair : Bodies::GetMasslessBodies()) {
-                AddNameSelectable(pair.first, pair.second.GetName());
-                AddMasslessMassText();
-                AddVelocityText(pair.second.GetVelocity());
+            bool comparison = id1.compare(id2);
+
+            if (sortSpecs->Specs->SortDirection == ImGuiSortDirection_Descending) {
+                return comparison;
+            } else {
+                return !comparison;
             }
+        }
 
-            ImGui::PopFont();
+        auto CompareBodyMasses(const string &id1, const string &id2) -> bool {
+            bool comparison = Bodies::GetBody(id1).GetMass() > Bodies::GetBody(id2).GetMass();
+            if (sortSpecs->Specs->SortDirection == ImGuiSortDirection_Descending) {
+                return comparison;
+            } else {
+                return !comparison;
+            }
+        }
+
+        auto CompareBodies(const string &id1, const string &id2) -> bool {
+            if (sortSpecs->Specs->ColumnIndex == NAME_COLUMN_ID) {
+                return CompareBodyNames(id1, id2);
+            } else {
+                return CompareBodyMasses(id1, id2);
+            }
         }
 
         auto AddTable() -> void {
             ImGui::Begin("Scenario Explorer", &windowOpen, WINDOW_FLAGS);
             ImGui::SetWindowSize(WINDOW_SIZE);
-            ImGui::BeginTable("scenario-explorer", 3, EXPLORER_FLAGS, EXPLORER_SIZE);
+            ImGui::BeginTable("scenario-explorer", 2, EXPLORER_FLAGS, EXPLORER_SIZE);
 
             ImGui::ShowDemoWindow();
 
             AddHeader();
-            AddMassiveBodies();
-            AddMasslessBodies();
+
+            sortSpecs = ImGui::TableGetSortSpecs();
+            vector<string> bodyIds;
+            AddAllBodies(bodyIds);
+            std::sort(bodyIds.begin(), bodyIds.end(), CompareBodies);
+            AddBodiesToTable(bodyIds);
 
             ImGui::EndTable();
             ImGui::End();
