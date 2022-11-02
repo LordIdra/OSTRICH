ce9517abf4fd94d0b3f0d1d9738fcf4c4f10c4f7
MetaMechanists <percy.baker.0@gmail.com>
2022-11-02 23:40:16 +0000

    Fixed linter warnings and added comments

--- src/rendering/interface/Explorer.cpp
+++ src/rendering/interface/Explorer.cpp
@@ -43,30 +43,42 @@ namespace ScenarioExplorer {
         bool windowOpen = true;
 
         auto LoadFonts() -> void {
+            // Load font config and set config options
             ImGuiIO& io = ImGui::GetIO();
             ImFontConfig config;
             config.MergeMode = true;
             config.GlyphMinAdvanceX = FONT_SIZE;
 
+            // Load 'main' font and merge icons
             mainFont = io.Fonts->AddFontFromFileTTF(MAIN_FONT_PATH.c_str(), FONT_SIZE);
             io.Fonts->AddFontFromFileTTF(ICON_FONT_PATH.c_str(), FONT_SIZE, &config, (const ImWchar*)ICON_RANGE);
-
+            
+            // Load 'data' font and merge icons
             dataFont = io.Fonts->AddFontFromFileTTF(DATA_FONT_PATH.c_str(), FONT_SIZE);
             io.Fonts->AddFontFromFileTTF(ICON_FONT_PATH.c_str(), FONT_SIZE, &config, (const ImWchar*)ICON_RANGE);
         }
 
         auto AddHeader() -> void {
+            // Use main font
             ImGui::PushFont(mainFont);
 
+            // Column header strings
             string nameColumn = ICON_MDI_FORMAT_TEXT + string(" Name");
             string massColumn = ICON_MDI_WEIGHT + string(" Mass");
-            string speedColumn = ICON_MDI_SPEEDOMETER_SLOW + string(" Speed");
 
+            // Freeze the top row so it doesn't disappear when scrolling
             ImGui::TableSetupScrollFreeze(0, 1);
+            
+            // Add name header
             ImGui::TableSetupColumn(nameColumn.c_str(), ImGuiTableColumnFlags_WidthFixed | ImGuiTableColumnFlags_DefaultSort, NAME_WIDTH, NAME_COLUMN_ID);
+
+            // Add mass header
             ImGui::TableSetupColumn(massColumn.c_str(), ImGuiTableColumnFlags_WidthStretch | ImGuiTableColumnFlags_PreferSortDescending, MASS_WIDTH, MASS_COLUMN_ID);
+
+            // Indicate to imgui that this is a header row
             ImGui::TableHeadersRow();
 
+            // Stop using main font
             ImGui::PopFont();
         }
 
@@ -94,7 +106,7 @@ namespace ScenarioExplorer {
 
         auto AddBodiesToTable(const vector<string> &bodyIds) -> void {
             ImGui::PushFont(dataFont);
-            for (const string id : bodyIds) {
+            for (const string &id : bodyIds) {
                 Body body = Bodies::GetBody(id);
                 AddNameSelectable(id, body.GetName());
                 if (body.GetMass() == 0) {
@@ -106,51 +118,55 @@ namespace ScenarioExplorer {
             ImGui::PopFont();
         }
 
-        auto CompareBodyNames(string id1, string id2) -> bool {           
-            for (char &c : id1) { c = toupper(c); }
-            for (char &c : id2) { c = toupper(c); }
+        auto CompareBodyNames(string id1, string id2) -> bool {
+            // Convert id1 and id2 to uppercase - necessary because string.compare is case-sensitive
+            for (char &c : id1) { c = char(toupper(c)); }
+            for (char &c : id2) { c = char(toupper(c)); }
 
-            bool comparison = id1.compare(id2);
-
+            bool comparison = id1.compare(id2) >= 0;
             if (sortSpecs->Specs->SortDirection == ImGuiSortDirection_Descending) {
                 return comparison;
-            } else {
-                return !comparison;
             }
+            return !comparison;
         }
 
         auto CompareBodyMasses(const string &id1, const string &id2) -> bool {
             bool comparison = Bodies::GetBody(id1).GetMass() > Bodies::GetBody(id2).GetMass();
             if (sortSpecs->Specs->SortDirection == ImGuiSortDirection_Descending) {
                 return comparison;
-            } else {
-                return !comparison;
             }
+            return !comparison;
         }
 
         auto CompareBodies(const string &id1, const string &id2) -> bool {
             if (sortSpecs->Specs->ColumnIndex == NAME_COLUMN_ID) {
                 return CompareBodyNames(id1, id2);
-            } else {
-                return CompareBodyMasses(id1, id2);
             }
+            return CompareBodyMasses(id1, id2);
         }
 
         auto AddTable() -> void {
+            // Begin
             ImGui::Begin("Scenario Explorer", &windowOpen, WINDOW_FLAGS);
             ImGui::SetWindowSize(WINDOW_SIZE);
             ImGui::BeginTable("scenario-explorer", 2, EXPLORER_FLAGS, EXPLORER_SIZE);
 
+            // For debugging purposes, will be removed later
             ImGui::ShowDemoWindow();
 
+            // Explorer header
             AddHeader();
 
+            // Sort body ids according to the columns the user has selected
             sortSpecs = ImGui::TableGetSortSpecs();
             vector<string> bodyIds;
             AddAllBodies(bodyIds);
             std::sort(bodyIds.begin(), bodyIds.end(), CompareBodies);
+
+            // Add said body ids to the table
             AddBodiesToTable(bodyIds);
 
+            // End
             ImGui::EndTable();
             ImGui::End();
         }
